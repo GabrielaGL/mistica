cea4a4d8c2a5de7aa880cc3bb4980497479cff4b
Gaabs <ggaabrielagomez@gmail.com>
2023-08-01 19:24:02 -0600

    Instalación de bootstrap

--- entiendeme/src/App.js
+++ entiendeme/src/App.js
@@ -1,9 +1,12 @@
+import React from 'react';
 import './assets/css/App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
     <div className="App">
-      Hola Mundo
+      <br/>
+      <button className='btn btn-primary'>Hola Mundo</button>
     </div>
   );
 }
